93952e3858d1d1e1435174b2c7b0b638146078a6
roguerogesh <rogesh062246@gmail.com>
2023-10-23 09:37:48 +0000

    solution

---
+++ app.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "userData.db");
+
+let database = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running At http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDbAndServer();
+
+app.post("/register", async (request, response) => {
+  let { username, name, password, gender, location } = request.body; //Destructuring the data from the API call
+
+  let hashedPassword = await bcrypt.hash(password, 10); //Hashing the given password
+
+  let checkTheUsername = `
+            SELECT *
+            FROM user
+            WHERE username = '${username}';`;
+  let userData = await database.get(checkTheUsername); //Getting the user details from the database
+  if (userData === undefined) {
+    //checks the condition if user is already registered or not in the database
+    /*If userData is not present in the database then this condition executes*/
+    let postNewUserQuery = `
+            INSERT INTO
+            user (username,name,password,gender,location)
+            VALUES (
+                '${username}',
+                '${name}',
+                '${hashedPassword}',
+                '${gender}',
+                '${location}'
+            );`;
+    if (password.length < 5) {
+      //checking the length of the password
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      /*If password length is greater than 5 then this block will execute*/
+
+      let newUserDetails = await database.run(postNewUserQuery); //Updating data to the database
+      response.status(200);
+      response.send("User created successfully");
+    }
+  } else {
+    /*If the userData is already registered in the database then this block will execute*/
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  let checkTheUsername = `
+            SELECT *
+            FROM user
+            WHERE username = '${username}';`;
+  const userData = await database.get(checkTheUsername);
+  if (userData === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, userData.password);
+    if (isPasswordMatched === true) {
+      response.send("Login success!");
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+app.put("/change-password", async (request, response) => {
+  const { username, oldPassword, newPassword } = request.body;
+  const checkForUserQuery = `
+  SELECT
+  *
+  FROM 
+  user
+  WHERE
+  username = ${username}`;
+  const dbUser = await database.get(checkForUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("User not registered");
+  } else {
+    const isValidPassword = await bcrypt.compare(oldPassword, dbUser.password);
+    if (isValidPassword === true) {
+      if (newPassword.length < 5) {
+        response.status(400);
+        response.send("Password is too short");
+      } else {
+        const encryptedPassword = await bcrypt.hash(newPassword, 10);
+        const updatePasswordQuery = `
+              UPDATE user
+              SET password = '${encryptedPassword}'
+              WHERE
+              username = '${username}'`;
+        await database.run(updatePasswordQuery);
+        response.send("Password updated");
+      }
+    } else {
+      response.status(400);
+      response.send("Invalid current password");
+    }
+  }
+});
+
+module.exports = app;
